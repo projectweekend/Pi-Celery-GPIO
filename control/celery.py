94be6ec12fd035960df3ade83eb6def9a62c364c
Brian Hines <brian@projectweekend.net>
2015-06-28 15:48:49 -0500

    Let's do this

--- control/celery.py
+++ control/celery.py
@@ -1,17 +1,34 @@
 from __future__ import absolute_import
 import os
 from celery import Celery
+from pi_pin_manager import PinManager
 
 
 RABBIT_URL = os.getenv('CONTROL_RABBIT_URL')
 assert RABBIT_URL
 
+CONTROL_PIN_CONFIG = os.getenv('CONTROL_PIN_CONFIG')
+assert CONTROL_PIN_CONFIG
 
-app = Celery('control', broker=RABBIT_URL, backend=RABBIT_URL, include=['control.tasks'])
+
+pins = PinManager(config_file=CONTROL_PIN_CONFIG)
+
+
+app = Celery('control', broker=RABBIT_URL, backend=RABBIT_URL)
 app.conf.update(
     CELERY_TASK_RESULT_EXPIRES=3600,
     BROKER_CONNECTION_TIMEOUT=10
 )
+
+
+@app.task
+def pin_on(pin_num):
+    pins.on(pin_num)
+
+
+@app.task
+def pin_off(pin_num):
+    pins.off(pin_num)
 
 
 if __name__ == '__main__':
